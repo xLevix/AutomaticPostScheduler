a57aede9f953ed263bf9bd0ab622b2810ccf0099
xLevix <senderpl12@gmail.com>
2023-03-10 02:10:44 +0100

    idk

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,34 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import LinkedIn from "next-auth/providers/linkedin";
+
+export default NextAuth({
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        LinkedIn({
+            clientId: process.env.LINKEDIN_ID,
+            clientSecret: process.env.LINKEDIN_SECRET,
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, user, account }) {
+            if (account) {
+                token.accessToken = account.access_token;
+            }
+            console.log("token", token);
+            return token;
+        },
+        async session({ session, token, }) {
+            if (session?.user) {
+                session.user.id = token.sub;
+            }
+            return {
+                ...session,
+                accessToken: token.accessToken,
+            };
+        },
+    }
+})
