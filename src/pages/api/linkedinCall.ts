ddf4867b0ab192be8ee33c42345ff4f759ba2133
xLevix <senderpl12@gmail.com>
2023-03-19 03:15:22 +0100

    linkedin support

--- src/pages/api/linkedinCall.ts
+++ src/pages/api/linkedinCall.ts
@@ -7,29 +7,29 @@ const handler = nc<NextApiRequest, NextApiResponse>()
         const { accessToken, text } = req.body; // pobieranie danych z ciała żądania
 
         // tutaj można wykonać jakieś operacje na danych, np. zapisać w bazie danych
+        const headers = {
+            ContentType: 'application/json',
+            LinkedInVersion: '202301',
+            XRestliProtocolVersion: '2.0.0',
+            Authorization: `Bearer ${accessToken}`
+        }
+
+        const data = {
+            "author": "urn:li:person:eJiIx5Mbul",
+            "commentary": `${text}`,
+            "visibility": "PUBLIC",
+            "distribution": {
+                "feedDistribution": "MAIN_FEED",
+                "targetEntities": [],
+                "thirdPartyDistributionChannels": []
+            },
+            "lifecycleState": "PUBLISHED",
+            "isReshareDisabledByAuthor": false
+        }
 
         // wysyłanie odpowiedzi z potwierdzeniem otrzymania danych
         console.log(accessToken, text)
-        axios.post('https://api.linkedin.com/rest/posts', {
-            headers: {
-                'Content-Type': 'application/json',
-                'LinkedIn-Version': '202301',
-                'X-Restli-Protocol-Version': '2.0.0',
-                'Authorization': `Bearer ${accessToken}`
-            },
-            data: {
-                "author": "urn:li:person:eJiIx5Mbul",
-                "commentary": `${text}`,
-                "visibility": "PUBLIC",
-                "distribution": {
-                    "feedDistribution": "MAIN_FEED",
-                    "targetEntities": [],
-                    "thirdPartyDistributionChannels": []
-                },
-                "lifecycleState": "PUBLISHED",
-                "isReshareDisabledByAuthor": false
-            }
-        })
+        axios.post('https://api.linkedin.com/rest/posts', data, { headers: headers})
             .then(function (response) {
                 console.log(response);
                 res.status(200).json({ message: 'success' })
