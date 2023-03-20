f2765883ddc2914b13d54200c6fe2f1784eb75fa
Pepe <senderpl12@gmail.com>
2023-03-20 15:08:31 +0100

    twitter test

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -30,9 +30,8 @@ export default NextAuth({
         }),
 
         TwitterProvider({
-            clientId: process.env.TWITTER_ID,
-            clientSecret: process.env.TWITTER_SECRET,
-            version: "2.0", // opt-in to Twitter OAuth 2.0
+            clientId: process.env.oauth_consumer_key,
+            clientSecret: process.env.oauth_consumer_secret,
         }),
 
         CredentialsProvider({
