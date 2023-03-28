206d9110e73477e905a0475b522b20c359f40e38
xLevix <senderpl12@gmail.com>
2023-03-28 20:27:35 +0200

    aws img upload test

--- src/pages/instaForm.js
+++ src/pages/instaForm.js
@@ -75,6 +75,38 @@ export default function Home() {
     };
 
 
+    const uploadPhoto = async (e) => {
+        const file = e.target.files?.[0];
+        const filename = encodeURIComponent(file.name)
+        const fileType = encodeURIComponent(file.type)
+
+        const res = await fetch(
+            `/api/upload-url?file=${filename}&fileType=${fileType}`
+        )
+        const {url, fields} = await res.json()
+        const formData = new FormData()
+
+        Object.entries({...fields, file}).forEach(([key, value]) => {
+            formData.append(key, value)
+        })
+
+        const upload = await fetch(url, {
+            method: 'POST',
+            body: formData,
+        })
+
+        if (upload.ok) {
+            console.log('Uploaded successfully!')
+            console.log('https://' + url.split('/')[3] + '.' + url.split('/')[2] + '/' + filename)
+            setImage('https://' + url.split('/')[3] + '.' + url.split('/')[2] + '/' + filename)
+
+        } else {
+            console.error('Upload failed.')
+        }
+    }
+
+
+
     return (
         <div>
             <h1>Send a message</h1>
@@ -102,35 +134,4 @@ export default function Home() {
             </form>
         </div>
     );
-}
-
-
-const uploadPhoto = async (e) => {
-    const file = e.target.files?.[0];
-    const filename = encodeURIComponent(file.name)
-    const fileType = encodeURIComponent(file.type)
-
-    const res = await fetch(
-        `/api/upload-url?file=${filename}&fileType=${fileType}`
-    )
-    const {url, fields} = await res.json()
-    const formData = new FormData()
-
-    Object.entries({...fields, file}).forEach(([key, value]) => {
-        formData.append(key, value)
-    })
-
-    const upload = await fetch(url, {
-        method: 'POST',
-        body: formData,
-    })
-
-    if (upload.ok) {
-        console.log('Uploaded successfully!')
-        console.log('https://' + url.split('/')[3] + '.' + url.split('/')[2] + '/' + filename)
-        return 'https://' + url.split('/')[3] + '.' + url.split('/')[2] + '/' + filename
-
-    } else {
-        console.error('Upload failed.')
-    }
 }
