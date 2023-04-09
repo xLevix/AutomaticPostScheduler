d144ceb03f13726c6360b522ba994ebdd43d5f37
xLevix <senderpl12@gmail.com>
2023-04-09 22:49:30 +0200

    added linkedin pictures support

---
+++ src/pages/api/qstashCall3.ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
+
+const handler = nc<NextApiRequest, NextApiResponse>()
+    .post((req, res) => {
+        const { accessToken, text, delay, img } = req.body;
+
+        var axios = require('axios');
+        var data = JSON.stringify({
+            accessToken: accessToken,
+            text: text,
+        });
+
+        if (img) {
+            data = JSON.stringify({
+                accessToken: accessToken,
+                text: text,
+                img: img
+            });
+        }
+
+        var config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'https://qstash.upstash.io/v1/publish/https://automatic-post-scheduler.vercel.app/api/twitterCall',
+            headers: {
+                'Content-Type': 'application/json',
+                'Upstash-Delay': `${delay}m`,
+                'Authorization': `Bearer ${process.env.QSTASH_TOKEN}`
+            },
+            data : data
+        };
+
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                res.status(200).json({ message: 'Post created' });
+            })
+            .catch(function (error) {
+                console.log(error);
+                res.status(500).json({ message: 'Post not created' });
+            });
+
+    });
+
+export default handler;
