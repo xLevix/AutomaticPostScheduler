d0f5481c4e09e45a06caeec21ea5ac055e3507d4
xLevix <senderpl12@gmail.com>
2023-10-14 02:11:18 +0200

    sse api gptcall

--- src/pages/api/gptCall.ts
+++ src/pages/api/gptCall.ts
@@ -1,79 +1,108 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
+import axios from 'axios';
+import cors from 'cors';
 
-const handler = nc<NextApiRequest, NextApiResponse>();
+const handler = nc<NextApiRequest, NextApiResponse>({
+    onError(error, req, res) {
+      res.status(500).end(`Something went wrong: ${error.message}`);
+    },
+    onNoMatch(req, res) {
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+    },
+});
 
-// Initialize rate limiting variables
-let lastRequestTime = 0;
-let requestCount = 0;
+handler.use(cors({ 
+    origin: '*',
+    methods: ['GET', 'HEAD', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}));
+
+const openaiAxios = axios.create({
+    baseURL: 'https://api.openai.com/v1/',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': process.env.OPENAI_API_KEY ? `Bearer ${process.env.OPENAI_API_KEY}` : ''
+    },
+});
+
+const rateLimitWindowMs = 20000;
+const maxRequestsPerWindow = 3;
+let requestTimestamps = [];
 
 handler.post(async (req, res) => {
-    // Check if rate limit has been exceeded
-    // const now = Date.now();
-    // if (now - lastRequestTime < 20000 && requestCount >= 3) {
-    //     res.status(429).json({ message: 'Rate limit exceeded. Please try again later.' });
-    //     return;
-    // }
+    console.log('Received payload:', req.body);
 
-    // // Update rate limiting variables
-    // lastRequestTime = now;
-    // requestCount++;
+    const now = Date.now();
+
+    requestTimestamps = requestTimestamps.filter(timestamp => now - timestamp < rateLimitWindowMs);
+
+    if (requestTimestamps.length >= maxRequestsPerWindow) {
+        res.status(429).json({ message: 'Rate limit exceeded. Please try again later.' });
+        return;
+    }
+
+    requestTimestamps.push(now);
 
     const { prompt } = req.body;
-    const axios = require('axios');
-    let data = {
+    const data = {
         model: "gpt-3.5-turbo",
-        stream: true,
         messages: [
             {
                 role: "system",
-                content: " You are assistant writing posts to social media like Instagram, Twitter and Linkedin. Write post in language that user asked question. Limit your post to maximum 2200 characters, can be less if you want."
+                content: "You are assistant writing posts to social media like Instagram, Twitter, and Linkedin. Write the post in the language that user asked the question. Limit your post to a maximum of 2200 characters; it can be less if you want."
             },
             {
                 role: "user",
                 content: prompt
             }
         ],
+        stream: true,
     };
 
-    let config = {
-        method: 'post',
-        url: 'https://api.openai.com/v1/chat/completions',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-        },
-        data: data,
-        responseType: 'stream'
-    };
+    // Set headers for SSE
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.flushHeaders();
 
-
-    axios.request(config)
-    .then((response) => {
-        response.data.on('data', (chunk) => {
-            const payloads = chunk.toString().split('\n\n');
-            for (const payload of payloads) {
-                if (payload.includes('[DONE]')) {
-                    res.end();
-                    return;
-                }
-                if (payload.startsWith('data:')) {
-                    const data = JSON.parse(payload.replace('data: ', ''));
-                    try {
-                        const text = data.choices[0].delta?.content;
-                        if (text) {
-                            res.write(`${text}`);
-                        }
-                    } catch (error) {
-                        console.error(`Error with JSON.parse and ${payload}.\n${error}`);
-                    }
-                }
+    function handleData(chunk, res) {
+        const payloads = chunk.toString().split('\n\n');
+        for (const payload of payloads) {
+            if (payload.includes('[DONE]')) {
+                res.write(`event: done\ndata: \n\n`);
+                res.flush();
+                res.end();
+                return;
             }
+            processPayload(payload, res);
+        }
+    }
+    
+    function processPayload(payload, res) {
+        if (!payload.startsWith('data:')) return;
+    
+        const data = JSON.parse(payload.replace('data: ', ''));
+        try {
+            const text = data.choices[0].delta?.content;
+            if (text) {
+                res.write(`data: ${text}\n\n`);
+                res.flush();
+            }
+        } catch (error) {
+            console.error(`Error with JSON.parse and ${payload}.\n${error}`);
+        }
+    }
+    
+    openaiAxios.post('chat/completions', data, { responseType: 'stream' })
+        .then((response) => {
+            response.data.on('data', (chunk) => handleData(chunk, res));
+        })
+        .catch((error) => {
+            console.error('Error when calling OpenAI:', error.response?.data);
+            res.status(500).json({ message: 'Internal Server Error', error: error.message });
         });
-    })
-    .catch((error) => {
-        res.status(500).json({ message: 'Internal Server Error', error: error.message });
-    });
 });
 
 export default handler;
