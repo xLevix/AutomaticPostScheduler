0b42d018744378360a7c9b51b04243149bf8b2a7
Nevemore <senderpl12@gmail.com>
2023-12-24 00:53:59 +0100

    fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@acme/ui', 'lodash-es'],
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        "fs": false,
+        "path": false,
+        "os": false,
+      }
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
